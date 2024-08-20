5231d2b272816160ac9a8e58c7d666b26bf0a48e
Rodrigo Toledo <rodrigotoledo@tjro.jus.br>
2024-08-20 12:34:30 -0400

    remove arquivos compilados e adiciona alterações

---
+++ src/PlanoOperadora.java
@@ -0,0 +1,18 @@
+public class PlanoOperadora {
+    
+    
+    public static void main(String[] args) {
+        String plano = "T"; //M / T
+        switch(plano){
+            case "T":{
+                System.out.println("5G Youtube");
+            }
+            case "M": {
+                System.out.println("WhatsApp e Instagram grátis");
+            }
+            case "B": {
+                System.out.println("100 minutos de ligação");
+            }
+        }
+    }
+}
