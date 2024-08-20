61c1708c0616f38b08a68a90f9d88c22e8e7ec9c
Rodrigo Toledo <rodrigotoledo@tjro.jus.br>
2024-08-20 11:17:56 -0400

    inclui arquivo SistemaMedida, estrutura Switch Case

---
+++ src/SistemaMedida.java
@@ -0,0 +1,33 @@
+public class SistemaMedida {
+    
+    public static void main(String[] args) {
+        String sigla = "G";
+        switch (sigla) {
+            case "P":{
+                System.out.println("Pequeno");
+                break;
+            }
+            case "M":{
+                System.out.println("Médio");
+                break;
+            }
+            case "G":{
+                System.out.println("Grande");
+                break;
+            }
+            default:
+                System.out.println("Indefinido");
+                break;
+        }
+        // if(sigla == "P"){
+        //     System.out.println("Pequeno");
+        // } else if(sigla == "M"){
+        //     System.out.println("Médio");
+        // } else if(sigla == "G"){
+        //     System.out.println("Grande");
+        // } else {
+        //     System.out.println("Indefinido");
+        // }
+    }
+    
+}
